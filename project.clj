44e434e4aa84b1e87826ca9c567d43b562ad0205
Ben Kovitz <bkovitz@indiana.edu>
2018-01-24 19:27:28 -0500

    project.clj

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject farg/util "0.1.0-SNAPSHOT"
+  :description "Generally useful utility functions from the FARG library."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[better-cond "2.0.1-SNAPSHOT"]
+                 [clj-time "0.14.2"]
+                 [com.rpl/specter "1.1.0"]
+                 [incanter "1.5.7"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/tools.macro "0.1.2"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.trace "0.7.9"]
+                 [org.clojure/core.async "0.3.443"]
+                 [net.mikera/core.matrix "0.61.0"]
+                 [popen "0.3.1"]
+                 [seesaw "1.4.5"]
+                 [ubergraph "0.4.0"]])
